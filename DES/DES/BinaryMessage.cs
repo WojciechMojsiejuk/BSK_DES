f5c5fc5021d1ae1c8fb74db23fda850d98ee7393
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2020-03-31 17:42:39 +0200

    init & write I/O

---
+++ DES/DES/BinaryMessage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DES
+{
+    public class BinaryMessage
+    {
+        public string BM_Filepath { get; set; }
+        public byte[] FileBytes { get; set; }
+        public byte[] OutputBytes { get; set; }
+
+        public BinaryMessage(string filepath)
+        {
+            BM_Filepath = filepath;
+        }
+        public void ReadFile()
+        {
+            try
+            {
+                FileBytes = File.ReadAllBytes(BM_Filepath);
+            }
+            catch(ArgumentException ae)
+            {
+                MessageBox.Show("Invalid path", ae.ToString(), MessageBoxButton.OK, MessageBoxImage.Error); 
+            }
+            catch(PathTooLongException ptle)
+            {
+                MessageBox.Show("Path too long", ptle.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                MessageBox.Show("Directory not found", dnfe.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                MessageBox.Show("File not found", fnfe.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (NotSupportedException nse)
+            {
+                MessageBox.Show("Invalid file format", nse.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (SecurityException se)
+            {
+                MessageBox.Show("No sufficient privilages to open file", se.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (IOException ioe)
+            {
+                MessageBox.Show("I/O Error", ioe.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void WriteFile(string filepath)
+        {
+            OutputBytes = FileBytes;
+            File.WriteAllBytes(filepath, OutputBytes);
+        }
+         
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in FileBytes)
+            {
+                sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ DES/DES/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DES
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private BinaryMessage binaryMessage;
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog.Filter = "Binary files (*.bin)|*.bin";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                binaryMessage = new BinaryMessage(openFileDialog.FileName);
+                filepath.Text = binaryMessage.BM_Filepath;
+                binaryMessage.ReadFile();
+                inputFileText.Text = binaryMessage.ToString();
+            }
+                
+
+        }
+
+        private void SaveFile(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "output"; 
+            saveFileDialog.DefaultExt = ".bin"; 
+            saveFileDialog.Filter = "Binary files (*.bin)|*.bin"; 
+
+            // Show save file dialog box
+            Nullable<bool> result = saveFileDialog.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = saveFileDialog.FileName;
+                binaryMessage.WriteFile(filename);
+            }
+        }
+    }
+}
